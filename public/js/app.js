9be0566a296e48eb545d9cb35bf40ee86f684690
Roberto Rugama <roberto.rugama01@gmail.com>
2018-03-18 07:23:21 -0600

    Load Lista Json
    loading a list de register whit a arrays usign the directive v-for de Vuejs and laravel

--- public/js/app.js
+++ public/js/app.js
@@ -12549,3 +12549,20 @@ return /******/ (function(modules) { // webpackBootstrap
 });
 ;
 //# sourceMappingURL=axios.map
+var urlUsers = 'https://jsonplaceholder.typicode.com/users';
+	new Vue({
+		el:'#main',
+		created: function(){
+			this.getUsers();
+		},
+		data: {
+			lists:[]
+		},
+		methods: {
+			getUsers: function(){
+				axios.get(urlUsers).then(response => {
+					this.lists=response.data
+				});
+			}
+		}
+	});
